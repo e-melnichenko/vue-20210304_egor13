43295da021b25f715fc631703aaa41e44611db3d
dinolike030 <egor.melnichenko@bk.ru>
2021-07-21 21:22:12 +0300

    07-forms 01-CounterButton_final

--- 07-forms/01-CounterButton_final/components/CounterButton.vue
+++ 07-forms/01-CounterButton_final/components/CounterButton.vue
@@ -1,9 +1,39 @@
 <template>
-  <button></button>
+  <button @click="handleClick">{{ localCount }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    }
+  },
+
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+
+  data() {
+    return {
+      localCount: this.count,
+    }
+  },
+
+  watch: {
+    count(newValue) {
+      this.localCount = newValue;
+    }
+  },
+
+  methods: {
+    handleClick() {
+      this.$emit('increment', this.localCount += 1);
+    }
+  }
 };
 </script>
