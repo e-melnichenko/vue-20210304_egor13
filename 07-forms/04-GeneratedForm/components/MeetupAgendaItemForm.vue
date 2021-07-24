36a5b513b8a72d2cb2c818147de3c6bfd704c3f6
dinolike030 <egor.melnichenko@bk.ru>
2021-07-24 16:04:47 +0300

    07-forms 04-GenetaredForm

--- 07-forms/04-GeneratedForm/components/MeetupAgendaItemForm.vue
+++ 07-forms/04-GeneratedForm/components/MeetupAgendaItemForm.vue
@@ -1,34 +1,52 @@
 <template>
   <DIV class="form-section form-section_inner">
-    <button type="button" class="remove-button">
-      <app-icon icon="trash" />
+    <button type="button" class="remove-button" @click="$emit('remove')">
+      <app-icon icon="trash"/>
     </button>
 
     <form-group>
-      <dropdown-button title="Тип" :options="$options.agendaItemTypeOptions" />
+      <dropdown-button
+        title="Тип"
+        :value="localAgendaItem.type"
+        :options="$options.agendaItemTypeOptions"
+        @change="(value) => updateAgendaItem('type', value)"
+      />
     </form-group>
 
     <div class="form__row">
       <div class="form__col">
         <form-group label="Начало">
-          <app-input type="time" placeholder="00:00" />
+          <app-input
+            type="time"
+            placeholder="00:00"
+            :value="localAgendaItem.startsAt"
+            @input="handleStartsAtChange"
+          />
         </form-group>
       </div>
       <div class="form__col">
         <form-group label="Окончание">
-          <app-input type="time" placeholder="00:00" />
+          <app-input
+            type="time"
+            placeholder="00:00"
+            :value="localAgendaItem.endsAt"
+            @input="(value) => updateAgendaItem('endsAt', value)"
+          />
         </form-group>
       </div>
     </div>
 
-    <form-group label="Заголовок">
-      <app-input />
-    </form-group>
-    <form-group label="Описание">
-      <app-input multiline />
-    </form-group>
-    <form-group>
-      <dropdown-button title="Язык" :options="$options.languageOptions" />
+    <form-group
+      v-for="item in $options.fieldSpecifications[localAgendaItem.type]"
+      :key="item.field"
+      :label="item.title"
+    >
+      <component
+        :is="item.component"
+        v-bind="item.props"
+        :[item.model.prop]="localAgendaItem[item.field]"
+        @[item.model.event]="(value) => updateAgendaItem(item.field, value)"
+      />
     </form-group>
   </DIV>
 </template>
@@ -48,6 +66,62 @@ export default {
   name: 'MeetupAgendaItemForm',
 
   components: { FormGroup, AppIcon, AppInput, DropdownButton },
+
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true,
+    }
+  },
+
+  data() {
+    return {
+      localAgendaItem: {...this.agendaItem},
+      currentStartsAt: null,
+    }
+  },
+
+  methods: {
+    updateAgendaItem(field, value) {
+      this.localAgendaItem[field] = value;
+
+      this.$emit('update:agendaItem', {
+        ...this.localAgendaItem,
+      });
+    },
+
+    handleStartsAtChange(value) {
+      // $event.target.valueAsNumber не проходит в тестах, тк в node нет valueAsNumber
+
+      const numValue = this.getNumberFromTimeString(value);
+      const delta = numValue - this.currentStartsAt;
+
+      const endsAtNumber = this.getNumberFromTimeString(this.localAgendaItem.endsAt);
+      const date = new Date(endsAtNumber + delta);
+      const hhmm = date.toISOString().slice(11, 16);
+
+      this.currentStartsAt = numValue;
+      this.localAgendaItem.startsAt = value;
+      this.localAgendaItem.endsAt = hhmm;
+
+      this.$emit('update:agendaItem', {
+        ...this.localAgendaItem,
+      })
+    },
+
+    getNumberFromTimeString(time) {
+      const [hours, minutes] = time.split(':');
+      const date = new Date(0);
+      date.setUTCHours(+hours);
+      date.setUTCMinutes(+minutes);
+
+      return +date
+    }
+  },
+
+  mounted() {
+    this.currentStartsAt = this.getNumberFromTimeString(this.localAgendaItem.startsAt)
+  },
 
   agendaItemTypeOptions: getAgendaItemTypeOptions(),
   fieldSpecifications: getAgendaItemsFieldSpecifications(),
